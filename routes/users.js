d0354000ad2504f96082a957e961bd9a88697878
amine kacem <maminekacem@hotmail.fr>
2016-04-05 11:57:27 +0100

    juste for test /i have added a comment

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,6 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+console.log('users rooot work fine');
 
 module.exports = router;
