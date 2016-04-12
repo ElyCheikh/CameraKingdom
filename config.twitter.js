fdca9f38568520a16539ad688324ba9d36091258
amine kacem <maminekacem@hotmail.fr>
2016-04-12 01:25:41 +0100

    config for twitter

---
+++ config.twitter.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+	twitter: {
+  	consumer_key: 'OT9yKj6vgB56IzLfa05GBUOqL',
+		consumer_secret: 'HTMDehwAb5ZYrBTwsgUK8a0pwCYzJq6em4xkhQb8NtNzoCUGI9',
+		access_token: '67004922-wDLTAthnGQvolkrlWgvhm0CPN3vpzuuXvekwACwkv',
+		access_token_secret: 'oNcxmCOifdWnpXoqEpbU1rTXHX6IZv2UiYC9t6M8Lqfon'
+	}
+
+};
